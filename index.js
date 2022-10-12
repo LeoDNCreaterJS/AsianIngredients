6ecd611dc45a1d8fd05506bb4a400d70f2aa5615
Leo DN <leodoan.081898@outlook.com>
2022-10-12 14:05:52 -0500

    third try

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var path = require('path');
 
 app.use("/public", express.static('assets'))
 
 app.get('/', (req, res) => {
-  res.sendFile("/assets/html/main.html")
-})
+  res.sendFile(path.join(__dirname,'/assets/html/main.html'))})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
